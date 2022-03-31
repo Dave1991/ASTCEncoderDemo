698aa03ff5b6052fc4eb93d87974176dd44dda88
forrestlin <forrestlin@tencent.com>
2022-03-31 22:14:13 +0800

    Initial Commit

---
+++ ASTCEncoderDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ASTCEncoderDemo
+//
+//  Created by forrest on 2022/3/31.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ASTCEncoderDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ASTCEncoderDemo
+//
+//  Created by forrest on 2022/3/31.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
